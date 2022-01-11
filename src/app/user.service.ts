f2b72ae95f6d10bf8b66252e739370eb340e04e6
kirannikam000061 <94921277+kirannikam000061@users.noreply.github.com>
2022-01-11 06:39:20 +0000

    minor fix

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -8,7 +8,7 @@ export class UserService {
 
   constructor(private _http:HttpClient) {}
     register(body:any){
-      return this._http.post('http://127.0.0.1:3000/user/register',body,{
+      return this._http.post('http://127.0.0.1:3000/users/register',body,{
         observe:'body',
         headers: new HttpHeaders().append('Content-Type', 'application/json')
       });
